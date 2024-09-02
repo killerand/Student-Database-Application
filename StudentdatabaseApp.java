e10bc83b5eb95503099b231a68990adf3950ca5e
Asiya Bint <163452363+killerand@users.noreply.github.com>
2024-09-02 17:19:22 +0900

    Create StudentdatabaseApp.java

---
+++ StudentdatabaseApp.java
@@ -0,0 +1,27 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class StudentdatabaseApp {
+
+	public static void main(String[] args) {
+// Ask how many new users we want to add
+System.out.print("Enter number of new students to enroll; ");
+Scanner in = new Scanner(System.in);
+int numOfStudents = in.nextInt();
+Student[] students = new Student[numOfStudents];
+
+// Create n number of new students 
+for (int n = 0; n < numOfStudents; n++) {
+students[n] = new Student() ;
+students[n].enroll();
+students[n].payTuition();
+
+}
+for (int n = 0; n < numOfStudents; n++) {
+System.out.println(students[n].toString());
+
+    }
+  }
+}
+
